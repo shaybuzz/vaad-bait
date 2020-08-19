362e1e79afb1f217e18842cee51ae9ad045bb096
Shay Buzz <shaybuzz@gmail.com>
2020-08-20 01:19:10 +0300

    filter title or details

--- src/components/MessagesPage.js
+++ src/components/MessagesPage.js
@@ -2,32 +2,49 @@ import React from 'react';
 import MessageComp from './MessageComp';
 import FiltersComp from "./FiltersComp";
 import Priority from '../model/Priority';
+import Message from '../model/Message';
 
 
-function MessagesPage(props){
-    const {messages} = props;
-    let allMsg = messages.map(msg => <MessageComp message={msg}/>)
+function MessagesPage(props) {
+    const { messages } = props;
+    const [theMessages, updateMessages] = React.useState(messages)
+    let messagesElements = theMessages.map(msg => <MessageComp message={msg} />)
+
 
     function onPriortyChange(priorty) {
-      if(priorty == Priority.Important)  {
-          console.log('filter Important')
-      }else if(priorty == Priority.Info){
-        console.log('filter info')
-      }else{
-        console.log('NO filter')
-      }
-    } 
+        if (priorty == Priority.Important) {
+            console.log('filter Important')
+            //TODO not working???
+            // updateMessages(messages.filter((msg) =>
+            //     msg.priorty === Priority.Important
+            // ))
+        } else if (priorty == Priority.Info) {
+            console.log('filter info')
+            // updateMessages(messages.filter((msg) =>
+            //     msg.priorty === Priority.Info
+            // ))
+        } else {
+            updateMessages(messages)
+        }
+    }
 
-    function onSearchMsg(msg) {
-        console.log(`search the msg ${msg}`)
+    function onSearchMsg(msgTxt) {
+        console.log(`search the msg ${msgTxt}`)
+        if (msgTxt === undefined || msgTxt === '') {
+            updateMessages(messages)
+        } else {
+            updateMessages(messages.filter((msg) =>
+                msg.title.includes(msgTxt) || msg.details.includes(msgTxt)
+            ))
+        }
 
-        
+
     }
 
     return <div>
         mesages page
-       <FiltersComp onFilterPriorty={onPriortyChange} onSearchText={onSearchMsg}/>
-        {allMsg}
+       <FiltersComp onFilterPriorty={onPriortyChange} onSearchText={onSearchMsg} />
+        {messagesElements}
     </div>
 
 }
